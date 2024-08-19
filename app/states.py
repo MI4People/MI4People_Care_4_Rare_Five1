4477d5a8b014f6bede92f7d013d4271230357274
johannesadam <johannes.adam@five1.de>
2024-08-19 15:02:19 +0200

    Fixed formatting

--- app/states.py
+++ app/states.py
@@ -14,7 +14,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from sklearn.preprocessing import MultiLabelBinarizer
 
-from utils import read_config,write_output
+from utils import read_config, write_output
+
 # ,CSVResultsBuilder,ResultRow
 from FeatureCloud.app.engine.app import AppState, app_state
 
@@ -23,15 +24,15 @@ logger = logging.getLogger(__name__)
 
 config = read_config()
 
-OUTPUT_DIR = '/mnt/output'
+OUTPUT_DIR = "/mnt/output"
 
-@app_state('initial')
+
+@app_state("initial")
 class ExecuteState(AppState):
 
     def register(self):
-        self.register_transition('terminal', Role.BOTH)
+        self.register_transition("terminal", Role.BOTH)
 
-        
     def run(self):
         # Get Neo4j credentials from config
         neo4j_credentials = config.get("neo4j_credentials", {})
@@ -40,7 +41,7 @@ class ExecuteState(AppState):
         NEO4J_PASSWORD = neo4j_credentials.get("NEO4J_PASSWORD", "")
         NEO4J_DB = neo4j_credentials.get("NEO4J_DB", "")
         logger.info(f"Neo4j Connect to {NEO4J_URI} using {NEO4J_USERNAME}")
-        
+
         # Driver instantiation
         driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USERNAME, NEO4J_PASSWORD))
 
@@ -53,22 +54,24 @@ class ExecuteState(AppState):
             logger.info("Fetching data from Neo4j: ...")
             fetcher = DataFetcher(session)
             logger.info("Fetching data from Neo4j: Done")
-            
+
             logger.info("Fetching validation data from Neo4j: ...")
             validationFetcher = ValidationDataFetcher(session)
             logger.info("Fetching validation data from Neo4j: Done")
-                
+
         data = [vars(obj) for obj in fetcher.subjects]
         df = pd.DataFrame(data)
-        df_A = df[['subjectId', 'isSick', 'icdFirstLetter', 'subjectMetrics', 'phenotypes']]
-        df_B = df[['subjectId', 'isSick', 'icdFirstLetter', 'subjectMetrics', 'phenotypes']]
-
+        df_A = df[
+            ["subjectId", "isSick", "icdFirstLetter", "subjectMetrics", "phenotypes"]
+        ]
+        df_B = df[
+            ["subjectId", "isSick", "icdFirstLetter", "subjectMetrics", "phenotypes"]
+        ]
 
         testdata = [vars(obj) for obj in validationFetcher.subjects]
         testdf = pd.DataFrame(testdata)
-        testdf_A = testdf[['subjectId', 'phenotypes', 'subjectMetrics']]
-        testdf_B = testdf[['subjectId', 'phenotypes', 'subjectMetrics']]
-
+        testdf_A = testdf[["subjectId", "phenotypes", "subjectMetrics"]]
+        testdf_B = testdf[["subjectId", "phenotypes", "subjectMetrics"]]
 
         result_A = randomForestA(df_A, testdf_A)
         logger.info(f"Results Task A: {result_A}")
@@ -78,11 +81,7 @@ class ExecuteState(AppState):
         logger.info(f"Results Task B: {result_B}")
         result_B.to_csv(f"{OUTPUT_DIR}/results_task_B.csv", index=False)
 
-
         # Close the driver connection
         driver.close()
 
-        return 'terminal'
-
-
-
+        return "terminal"
